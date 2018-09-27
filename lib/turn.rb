d93b19e8eb36ef0e081ef730d323837cf3dccaea
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-09-27 19:12:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,4 @@
-def display_board(board)    
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -17,7 +17,7 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  else 
+  else
     true
   end
 end
